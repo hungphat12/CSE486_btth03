40ba2cc9a0a072bc0d354b57fd4df5b9f2468e78
Phan Phát <90019371+hungphat12@users.noreply.github.com>
2023-03-13 15:30:29 +0700

    Add files via upload

---
+++ BT1/register.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Đăng ký</title>
+</head>
+<body>
+	<h2>Đăng ký</h2>
+	<form method="POST" action="register.php">
+		<label for="email">Email:</label>
+		<input type="email" name="email" id="email" required><br><br>
+
+		<label for="password">Password:</label>
+		<input type="password" name="password" id="password" required><br><br>
+
+		<input type="submit" name="register" value="Đăng ký">
+	</form>
+</body>
+</html>
